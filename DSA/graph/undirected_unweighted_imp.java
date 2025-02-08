239b04d64137fb70d9268a7f330873d771dc8760
rajitchadar59 <rajitathiya123@gmail.com>
2025-02-08 19:28:15 +0530

    graph files added

--- DSA/graph/undirected_unweighted_imp.java
+++ DSA/graph/undirected_unweighted_imp.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class implimentation_method_unweighted {
+public class undirected_unweighted_imp {
    static class edge{
     int src;
     int des;
---
+++ DSA/graph/undirected_weighted_imp.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+public class undirected_weighted_imp {
+   static class edge{
+    int src;
+    int des;
+    int wt;
+    public edge(int s , int d ,int w){
+        this.src=s;
+        this.des=d;
+        this.wt=w;
+    }
+   }
+   public static void create_graph(ArrayList<edge> graph[]){
+   for(int i =0 ; i<graph.length ; i++){
+    graph[i]=new ArrayList<>();
+   }
+   graph[0].add(new edge(0,2,2));
+   graph[1].add(new edge(1,2,10));
+   graph[1].add(new edge(1,3,0));
+   graph[2].add(new edge(2,0,2));
+   graph[2].add(new edge(2,1,10));
+   graph[2].add(new edge(2,3,-1));
+   graph[3].add(new edge(3,1,0));
+   graph[3].add(new edge(3,2,-1));
+   }
+    public static void main(String[] args) {
+        int v=10;
+        ArrayList<edge> graph[] = new ArrayList[v];
+        create_graph(graph);
+       //print 2's neighbour
+       for(int i=0 ; i<graph[2].size() ;i++){
+        edge e=graph[2].get(i);
+        System.out.println(e.des+" , "+e.wt);
+       }
+
+    }
+} 
